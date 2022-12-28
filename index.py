10c1254090b70efcebcd7d9d806d141091770c08
Clive D <cdarra@github.com>
2022-12-28 17:38:11 +0000

    add spaces

--- index.py
+++ index.py
@@ -6,9 +6,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Hello, World! '+datetime.now().strftime("%c")
+    return 'Hello, World! ' + datetime.now().strftime("%c")
 
 @app.route('/about')
 def about():
-    return 'About '+datetime.now().strftime("%c")
-    
+    return 'About ' + datetime.now().strftime("%c")
