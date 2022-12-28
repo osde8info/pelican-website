9827163e7c9889dd5acee7df25705417f4b3b7ab
Clive D <cdarra@github.com>
2022-12-28 21:20:53 +0000

    pelcian run

--- index.py
+++ index.py
@@ -1,13 +1,41 @@
-from datetime import datetime
+# -*- coding: utf-8 -*-
 
-from flask import Flask
+import os
+import shlex
+import shutil
+import sys
+import datetime
 
-app = Flask(__name__)
+from invoke import task
+from invoke.main import program
+from invoke.util import cd
 
-@app.route('/')
-def home():
-    return 'Hello, World! ' + datetime.now().strftime("%c")
+from pelican import main as pelican_main
+from pelican import run as pelican_run
 
-@app.route('/about')
-def about():
-    return 'About ' + datetime.now().strftime("%c")
+from pelican.server import ComplexHTTPRequestHandler, RootedHTTPServer
+from pelican.settings import DEFAULT_CONFIG, get_settings_from_file
+
+OPEN_BROWSER_ON_SERVE = True
+SETTINGS_FILE_BASE = 'pelicanconf.py'
+SETTINGS = {}
+SETTINGS.update(DEFAULT_CONFIG)
+LOCAL_SETTINGS = get_settings_from_file(SETTINGS_FILE_BASE)
+SETTINGS.update(LOCAL_SETTINGS)
+
+CONFIG = {
+    'settings_base': SETTINGS_FILE_BASE,
+    'settings_publish': 'publishconf.py',
+    # Output path. Can be absolute or relative to tasks.py. Default: 'output'
+    'deploy_path': SETTINGS['OUTPUT_PATH'],
+    # Host and port for `serve`
+    'host': 'localhost',
+    'port': 8000,
+}
+
+def build(c):
+    pelican_run('-s {settings_base}'.format(**CONFIG))
+
+
+build(1)
+
